6b3cf2f6916aff8609737c112900a70111e1a0d8
fkdkdsj <32150887+fkdkdsj@users.noreply.github.com>
2018-06-02 14:53:53 +0800

    Update json2csv.py

--- json2csv.py
+++ json2csv.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 def json2csv():
     try:
-        json_file = input('请输入要解析的json文件名，json文件必须和当前程序同一目录下：') + 'json'
+        json_file = input('请输入要解析的json文件名，json文件必须和当前程序同一目录下：') + '.json'
         if os.path.exists(sys.path[0] + '/' + json_file) == True:
             data = pd.read_json(json_file, encoding="utf8") # 编码很重要，否则会报到错    
         else:
             print('当前目录并没有该文件，请重新运行该程序')
         
-        csv_file = input('请输入要导出的csv文件名，文件保存在前程序同一目录下：') + 'csv'
+        csv_file = input('请输入要导出的csv文件名，文件保存在前程序同一目录下：') + '.csv'
         if csv_file.endswith('csv') == False:
             print('???开玩笑么，导出格式不是csv')
         else:
